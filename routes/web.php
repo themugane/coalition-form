a6fec095b6f07a81f83be5770f5690b77c60810a
themugane <themugane@gmail.com>
2023-01-26 11:25:36 +0300

    Route -> controller -> view workflow

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FormController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get("/", [FormController::class, 'index'])->name('coalition_form');
